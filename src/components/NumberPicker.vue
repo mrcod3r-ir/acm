6de07b3f633ab60e7bf1332a4dc4e3bc9bc16fd7
mr cod3r <mrcod3r.ir@gmail.com>
2022-03-24 13:50:03 +0430

    init

---
+++ src/components/NumberPicker.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<button @click="plus">+</button>
+		{{ modelValue }}
+		<button @click="minus">-</button>
+	</div>
+</template>
+
+<script lang="ts">
+	import { defineComponent } from "vue"
+	export default defineComponent({
+		props: {
+			modelValue: { type: Number, default: 0 }
+		},
+		emits: ["update:modelValue"],
+		methods: {
+			plus() {
+				this.$emit("update:modelValue", this.modelValue + 1)
+			},
+			minus() {
+				this.$emit("update:modelValue", this.modelValue - 1)
+			}
+		}
+	})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { defineComponent } from "vue"
+	// import TheWelcome from "@/components/TheWelcome.vue"
+	import NumberPicker from "@/components/NumberPicker.vue"
+	export default defineComponent({
+		name: "Home",
+		components: {
+			NumberPicker
+		},
+		data() {
+			return { counter: 0 }
+		}
+	})
+</script>
+
+<template>
+	<main>
+		<!-- <TheWelcome /> -->
+		<number-picker v-model:modelValue="counter"></number-picker>
+	</main>
+</template>
