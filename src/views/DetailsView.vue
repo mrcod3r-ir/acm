c207055da5817281afef00d17e25a03bfb0cd32d
mr cod3r <mrcod3r.ir@gmail.com>
2022-03-28 12:20:50 +0430

    complete until part 20

---
+++ src/views/DetailsView.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { useRoute } from "vue-router"
+	import { defineComponent } from "vue"
+
+	export default defineComponent({
+		name: "Details",
+		setup() {
+			const route = useRoute()
+			console.log("route.params.id", route.params.id)
+			console.log("query ", route.query)
+			return {}
+		}
+	})
+</script>
+<template>
+	<div class="details">
+		<h1>This is an details page</h1>
+	</div>
+</template>
+
+<style>
+	@media (min-width: 1024px) {
+		.details {
+			min-height: 100vh;
+			display: flex;
+			align-items: center;
+		}
+	}
+</style>
