a03ebd40a325691f9169aed74f8571e420f54af8
mr cod3r <mrcod3r.ir@gmail.com>
2022-03-30 10:01:03 +0430

    p21  add slot and card.vue

---
+++ src/components/Card.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="card-container">
+		<p>
+			<slot></slot>
+		</p>
+		<div>
+			<slot name="actionButtons" counter="5"></slot>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+	import { defineComponent } from "vue"
+	import { useCounter } from "@/composables/userCounter"
+
+	export default defineComponent({
+		setup() {
+			const { counter, plus, state, plusState, computdCounter } = useCounter()
+
+			return { counter, plus, state, plusState, computdCounter }
+		}
+	})
+</script>
+
+<style scoped>
+	.card-container {
+		border: 2px solid rgb(207, 207, 207);
+		max-width: 20rem;
+		padding: 1rem;
+	}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,11 +3,13 @@
 	// import TheWelcome from "@/components/TheWelcome.vue"
 	import NumberPicker from "@/components/NumberPicker.vue"
 	import counterVue from "@/components/counter.vue"
+	import Card from "@/components/Card.vue"
 	export default defineComponent({
 		name: "Home",
 		components: {
 			NumberPicker,
-			counterVue
+			counterVue,
+			Card
 		},
 		data() {
 			return { counter: 0 }
@@ -20,5 +22,15 @@
 		<!-- <TheWelcome /> -->
 		<number-picker v-model:modelValue="counter"></number-picker>
 		<counter-vue></counter-vue>
+		<br />
+		<card>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque distinctio doloribus ex, asperiores deleniti assumenda quis, commodi odio, cupiditate illo placeat in. Iusto corrupti blanditiis
+			qui ipsam. Alias, impedit nulla.
+			<template #actionButtons="{ counter }">
+				{{ counter }}
+				<button>submit</button>
+				<button>info</button>
+			</template>
+		</card>
 	</main>
 </template>
